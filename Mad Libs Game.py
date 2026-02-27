572f6c41a8d3b9954fb4cdb0d4711ee256474d62
sudoDr <91832908+sudoDr@users.noreply.github.com>
2026-02-27 12:46:32 +0300

    Add files via upload

---
+++ Mad Libs Game.py
@@ -0,0 +1,7 @@
+color = input (" write a color: " )
+plural_name = input (" write a plural name: " )
+celebrity = input (" write a celebrity: " )
+
+print (" Roses are " + color)
+print (plural_name + " are blue !")
+print (" I Love " + celebrity)
